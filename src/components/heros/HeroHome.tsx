a9adae0cb023c9bcf7ea3da01c647290c9bbb07a
Arjan Ceelen <arjan@celar.nl>
2025-10-08 08:27:18 +0200

    replaced hero image homepage

--- src/components/heros/HeroHome.tsx
+++ src/components/heros/HeroHome.tsx
@@ -2,27 +2,25 @@
 import { twMerge } from "tailwind-merge";
 import Image from "next/image";
 
+import heroImg from '../../../public/images/leren-is-leuk-als-je-het-snapt.png';
+
 export function HeroHome({ className }: { className?: string }) {
     return (
         <div className="relative">
-            <div className={twMerge("base-container", className)}>
-                <Image src="/images/logo-element-2.svg" className="absolute -bottom-[80px] right-1/3" alt="BSC logo element" width={374} height={179} />
-                <Image src="/images/logo-element-1.svg" className="absolute top-[140px] z-1 left-0" alt="BSC logo element" width={144} height={177} />
-                <div className="relative z-0 mt-[40px]">
-                    <div className="absolute z-2 w-full flex justify-center h-[calc(100%+120px)] -top-[120px] overflow-hidden">
-                        <div className="relative w-full h-full">
-                            <Image 
-                                src="/images/girl.png" 
-                                alt="Woman with books" 
-                                fill
-                                sizes="(max-width: 1024px) 30vw, 50vw"
-                                className="object-contain scale-[1.3] lg:scale-[1.5] translate-y-[25%]"
+            <div className={twMerge("base-container mb-24", className)}>
+                <Image src="/images/logo-element-2.svg" className="hidden md:block absolute -bottom-[80px] right-1/3" alt="BSC logo element" width={374} height={179} />
+                <Image src="/images/logo-element-1.svg" className="scale-[0.5] origin-top-left lg:scale-[1] absolute top-[140px] left-0" alt="BSC logo element" width={144} height={177} />
+                <div className="relative z-0 lg:-mt-[30px]">
+                    <div className="z-2 aspect-[22/9]">
+                             <Image 
+                                src={heroImg}
+                                alt="Woman with books"
+                                width={1112}
+                                height={498}
+                                sizes="(max-width: 640px) 100vw, (max-width: 1024px) 90vw, (max-width: 1280px) 60vw, 50vw"
+                                className="object-contain"
                                 priority
                             />
-                        </div>
-                    </div>
-                    <div className="relative z-1 aspect-[22/9] mb-[90px] rounded-xl overflow-hidden bg-[linear-gradient(to_right,_transparent,_#53769b),radial-gradient(circle,_#bdcddb,_#80a0be)]">
-                        <Image src="/images/logo-element-3.svg" className="absolute top-[20px] -right-[30px]" alt="BSC logo element" width={167} height={89} />
                     </div>
                 </div>
             </div>
