87dd81c6fc11a8e867045fd2bbc2f78c154c3c1e
Arjan Ceelen <arjan@celar.nl>
2026-02-15 08:57:26 +0100

    font issues fixed for safari

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -6,6 +6,41 @@ export const CocoGooseFont = localFont({
       path: '../../public/fonts/Cocogoose-Pro-Bold.woff2',
       weight: '700',
       style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Cocogoose-Pro-Bold.woff',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Cocogoose-Pro-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Cocogoose-Pro-Bold.otf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Cocogoose-Pro-Bold.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Cocogoose-Pro-Bold.woff',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Cocogoose-Pro-Bold.ttf',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Cocogoose-Pro-Bold.otf',
+      weight: '900',
+      style: 'normal',
     }
   ],
   display: 'swap',
